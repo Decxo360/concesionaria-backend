e5a78234aed5179fc3a789fb22634b268599d114
Decxo360 <diegolundstedt99@gmail.com>
2023-09-18 17:38:33 -0300

    refactor

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\ColorController;
 use App\Http\Controllers\LivianoController;
 use App\Http\Controllers\VehiculoController;
 
+// Route::resource('vehiculos/pdf',[VehiculoController::class,'pdf'])->name('vehiculos.pdf');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
 Route::apiResource('colores', ColorController::class);
 Route::apiResource('livianos', LivianoController::class);
-
 //Rutas por defecto creados por los controladores de cada recurso, en este caso cuenta con 4 metodos GET, POST, DELETE, PUT/PATCH
 //only -> indica que métodos serán intermediados por un middleware
 //Tiene un middleware llamado Throttle el cual te permite realizar 10 peticiones cómo máximo en un minuto
 Route::apiResource('vehiculos', VehiculoController::class)
     ->only(['index','store'])
     ->middleware('throttle:10,1');
+
+
